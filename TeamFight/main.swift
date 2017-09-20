4ad954239205964988973cd01a00e69f43a9e16e
mabayon <mabayon@student.42.fr>
2017-09-20 16:17:07 +0200

    Initial Commit

---
+++ TeamFight/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TeamFight
+//
+//  Created by Mahieu Bayon on 20/09/2017.
+//  Copyright © 2017 Mahieu Bayon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
